fc134f7b80ff1bf0a3e955f6d61ffb0af7568c08
darkbasic2000 <marcio.vieira.scea@gmail.com>
2022-12-03 13:29:13 -0400

    Arquivos JavaScript

---
+++ js/login.js
@@ -0,0 +1,59 @@
+$(document).ready(function () {
+    $('#matricula').focus();
+    $('#matricula').mask('999.999.99-a');
+    $('#matricula').blur(function() {
+        $('#matricula').val($('#matricula').val().toUpperCase());
+    });
+    $('#acessar').click(function () {
+        var matricula = $('#matricula').val().trim();
+        var senha = $('#senha').val();
+        var csrf_token = $('#csrf_token').val();
+        if(matricula === '') {
+            $('.alert').show();
+            $('.alert-message').text("Insira a matrícula");
+            $('#matricula').focus();
+        }
+        else if(senha === '') {
+            $('.alert').show();
+            $('.alert-message').text("Insira a senha");
+            $('#senha').focus();
+        }
+        else {
+            $.ajax({
+                method: 'POST',
+                url: 'controllers/FuncionarioController.php',
+                data: {'query': 'login', 'matricula': matricula, 'senha': senha, 'csrf_token':csrf_token},
+                dataType: 'json'
+            })
+            .done(function (data) {
+                if(data === true) {
+                    window.location = "views/dashboard.php";
+                }
+                else {
+                    $('.alert').show();
+                    $('.alert-message').text("Matrícula e/ou senha incorretas");
+                    $('#matricula').focus();
+                }
+            });
+        }
+    });
+});
+
+function editar(id) {
+    window.location.href = "cargos_edita.php?id=" + id;
+}
+
+function deletar(id) {
+    
+    $.ajax({
+        method: 'POST',
+        url: '../controllers/CargoController.php',
+        data: {'query': 'deletar', 'id': id},
+        dataType: 'json'
+    }).done(function (data) {
+        if(data === true) {
+            window.location.href = "cargos.php";
+        }
+    });
+
+}
